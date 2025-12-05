f5a0e4ea1c02db462740f757226f8712be90b30e
RojanShrestha77 <shresrozan555@gmail.com>
2025-12-05 20:29:41 +0545

    Add Register Screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hamro_deal/screens/login_screen.dart';
 import 'package:hamro_deal/widgets/my_button.dart';
 import 'package:hamro_deal/widgets/my_textformfield.dart';
 
@@ -21,6 +22,31 @@ class RegisterScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              SizedBox(height: 70),
+              Image.asset(
+                'assets/images/registerlogo.png',
+                width: 250,
+                height: 250,
+                fit: BoxFit.contain,
+              ),
+              SizedBox(height: 25),
+              Text(
+                "Register",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF1E1E1E),
+                  // letterSpacing: 1.2,
+                ),
+              ),
+
+              // const SizedBox(height: 20),
+              Text(
+                "Create your new account",
+                style: TextStyle(fontSize: 18, color: Color(0xFF1E1E1E)),
+              ),
+              const SizedBox(height: 25),
+
               MyTextformfield(
                 firstcontroller: fullNameController,
                 text: "Full Name",
@@ -40,7 +66,7 @@ class RegisterScreen extends StatelessWidget {
                 text: "password",
               ),
 
-              const SizedBox(height: 20),
+              const SizedBox(height: 100),
 
               MyButton(
                 onPressed: () {
@@ -58,10 +84,37 @@ class RegisterScreen extends StatelessWidget {
                     print('Password: $fullName');
                   }
                 },
-                text: "Login",
+                text: "Register",
                 width: double.infinity,
-                height: 50,
-                color: Colors.blue,
+                height: 65,
+                // color: Colors.blue,
+              ),
+
+              const SizedBox(height: 10),
+
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Already have an account?",
+                    style: TextStyle(fontSize: 17, color: Color(0xFF1E1E1E)),
+                    children: [
+                      TextSpan(
+                        text: "Sign up",
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Color(0xFF147AFF),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
