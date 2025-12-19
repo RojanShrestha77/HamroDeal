e01f8c029dbde57a529eccaaf688c6673310d457
RojanShrestha77 <shresrozan555@gmail.com>
2025-12-19 08:09:48 +0545

    sprint2

--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -11,5 +11,18 @@ ThemeData getApplicationTheme() {
         fontWeight: FontWeight.w600,
       ),
     ),
+
+    appBarTheme: AppBarTheme(
+      foregroundColor: Colors.black,
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        fontFamily: 'Jost SemiBold',
+        color: Colors.black,
+        fontSize: 22,
+        letterSpacing: 0.5,
+        // fontWeight: FontWeight.bold,
+      ),
+    ),
   );
 }
