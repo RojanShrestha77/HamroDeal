97b1d6c7c1e8296804fde09cd608ab7e157858fe
RojanShrestha77 <shresrozan555@gmail.com>
2026-01-02 14:08:44 +0545

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-// main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hamro_deal/app.dart'; // Your MyApp widget
+import 'package:hamro_deal/app.dart';
 import 'package:hamro_deal/core/services/hive_service.dart';
 
 Future<void> main() async {
