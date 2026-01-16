c7905db4fe521ab0c3e2858af2c9433558cc3e80
RojanShrestha77 <shresrozan555@gmail.com>
2026-01-16 17:22:22 +0545

    Netwrok check

---
+++ lib/core/api/api_endpoints.dart
@@ -0,0 +1,35 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  // BASE URl - change this for production
+  static const String baseUrl = 'http://10.0.2.2:3000/api/v1';
+
+  static const Duration connectionTimeout = Duration(seconds: 30);
+  static const Duration receiveTimeout = Duration(seconds: 30);
+
+  // ====== Batch Endpoints =========
+  static const String batches = '/batchs';
+  static String batchById(String id) => '/batches/$id';
+
+  // ====== category Endpoints ======
+  static const String categories = '/categories';
+  static String categoryById(String id) => '/categories/$id';
+
+  // ======== Student Endpoints ========
+  static const String students = '/students';
+  static const String studentLogin = '/students/login';
+  static const String studentRegister = '/students/register';
+  static String studentById(String id) => '/students/$id';
+  static String studentPhoto(String id) => '/students/$id/photo';
+
+  // ======= Item Endpoints =========
+  static const String items = '/items';
+  static String itemById(String id) => '/items/$id';
+  static String itemClaim(String id) => '/items/$id/claim';
+
+  // ========= comment Endpoints =========
+  static const String comments = '/comments';
+  static String commentById(String id) => '/comments/$id';
+  static String commentsByItem(String itemId) => '/comments/items/$itemId';
+  static String commentLike(String id) => '/comments/$id/like';
+}
---
+++ lib/core/services/connectivity/network_info.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+abstract interface class INetworkInfo {
+  Future<bool> get isConnected;
+}
+
+// provider
+final networkInfoProvider = Provider<NetworkInfo>((ref) {
+  return NetworkInfo(Connectivity());
+});
+
+class NetworkInfo implements INetworkInfo {
+  final Connectivity _connectivity;
+
+  NetworkInfo(this._connectivity);
+
+  @override
+  Future<bool> get isConnected async {
+    final result = await _connectivity
+        .checkConnectivity(); //check either the wifi or the mobile data is turned on?
+    if (result.contains(ConnectivityResult.none)) {
+      return false;
+    }
+    return true;
+  }
+
+  // check either we have the internet or not
+  Future<bool> _sachikaiInternetChakiNai() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } catch (e) {
+      return false;
+    }
+  }
+}
