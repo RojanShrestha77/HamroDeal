f2c2ad4160437e633cccbd0d6dc55cd44a525b27
RojanShrestha77 <shresrozan555@gmail.com>
2026-01-21 17:33:47 +0545

    token service

--- lib/features/auth/data/datasources/remote/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/remote/auth_remote_datasource.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hamro_deal/core/api/api_client.dart';
 import 'package:hamro_deal/core/api/api_endpoints.dart';
+import 'package:hamro_deal/core/services/storage/token_service.dart';
 import 'package:hamro_deal/core/services/storage/user_session_service.dart';
 import 'package:hamro_deal/features/auth/data/datasources/auth_datasource.dart';
 import 'package:hamro_deal/features/auth/data/models/auth_api_model.dart';
@@ -10,18 +11,22 @@ final authRemoteDatasourceProvider = Provider<IAuthRemoteDatasource>((ref) {
   return AuthRemoteDatasource(
     apiClient: ref.read(apiClientProvider),
     userSessionService: ref.read(userSessionServiceProvider),
+    tokenService: ref.read(tokenServiceProvider),
   );
 });
 
 class AuthRemoteDatasource implements IAuthRemoteDatasource {
   final ApiClient _apiClient;
   final UserSessionService _userSessionService;
+  final TokenService _tokenService;
 
   AuthRemoteDatasource({
     required ApiClient apiClient,
     required UserSessionService userSessionService,
+    required TokenService tokenService,
   }) : _userSessionService = userSessionService,
-       _apiClient = apiClient;
+       _apiClient = apiClient,
+       _tokenService = tokenService;
 
   @override
   Future<AuthApiModel?> getUserById(String authId) {
@@ -46,6 +51,9 @@ class AuthRemoteDatasource implements IAuthRemoteDatasource {
         fullName: user.fullName,
         username: user.username,
       );
+      // save token
+      final token = response.data['token'] as String?;
+      await _tokenService.saveToken(token!);
       return user;
     }
     return null;
