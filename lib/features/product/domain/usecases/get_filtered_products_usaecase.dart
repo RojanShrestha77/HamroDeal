c66666d917da2e460c71f487cf829ef5bb01192a
RojanShrestha77 <shresrozan555@gmail.com>
2026-02-20 15:59:00 +0545

    updated filter features

---
+++ lib/features/product/domain/usecases/get_filtered_products_usaecase.dart
@@ -0,0 +1,59 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:hamro_deal/core/error/failures.dart';
+import 'package:hamro_deal/core/usecase/app_usecase.dart';
+import 'package:hamro_deal/features/product/domain/entities/product_entity.dart';
+import 'package:hamro_deal/features/product/domain/repositories/product_repository.dart';
+
+class GetFilteredProductsParams extends Equatable {
+  final String? categoryId;
+  final String? search;
+  final double? minPrice;
+  final double? maxPrice;
+  final String? sort;
+
+  const GetFilteredProductsParams({
+    this.categoryId,
+    this.search,
+    this.minPrice,
+    this.maxPrice,
+    this.sort,
+  });
+
+  // helper methiod to check if any filter is applied
+  bool get hasFilters =>
+      categoryId != null ||
+      search != null ||
+      minPrice != null ||
+      maxPrice != null ||
+      sort != null;
+
+  //  create empty filter
+  factory GetFilteredProductsParams.empty() =>
+      const GetFilteredProductsParams();
+
+  @override
+  List<Object?> get props => [categoryId, search, minPrice, maxPrice, sort];
+}
+
+class GetFilteredProductsUsecase
+    implements
+        UsecaseWithParams<List<ProductEntity>, GetFilteredProductsParams> {
+  final IProductRepository _productRepository;
+
+  GetFilteredProductsUsecase({required IProductRepository productRepository})
+    : _productRepository = productRepository;
+
+  @override
+  Future<Either<Failure, List<ProductEntity>>> call(
+    GetFilteredProductsParams params,
+  ) {
+    return _productRepository.getFilteredProducts(
+      categoryId: params.categoryId,
+      search: params.search,
+      minPrice: params.minPrice,
+      maxPrice: params.maxPrice,
+      sort: params.sort,
+    );
+  }
+}
